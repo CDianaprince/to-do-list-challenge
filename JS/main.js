64fac5068566eb704e3e22e2e7e3771459b9162e
unknown <dcarol.prince@gmail.com>
2022-12-26 21:08:29 -0600

    funcionalidad del input para agragar en la lista

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,6 @@
 // Imports de nuestro modulos
 import Items from "./modules/add-remove.js";
-import ToDoItem from "./modules/item-constructor.js";
+/*import ToDoItem from "./modules/item-constructor.js";*/
 
 // Constantes iniciales
 const main = document.getElementById('main');
@@ -8,39 +8,37 @@ const form = document.getElementById('form');
 
 const inputDescription = document.getElementById('todo-item');
 
-const itemsContainer = document.getElementById('todo-cont');
+const itemsContainer = document.getElementById('todo-items');
 
-// Evento de escucha de la forma
+//consturctor en clase
+class ToDoItem {
+  constructor(completed, description, index){
+    this.completed = completed;
+    this.description = description;
+    this.index = index;
+  }
+}
+
+
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 });
 
-// Evento de escucha main para ingresar elementos
 main.addEventListener('keypress', (e) => {
-  // Obtener los valores
-  const description = inputDescription.value;
+ //obtener valore
+ const description = inputDescription.value;
 
-  // Validacion
-  if(description == '') {
-    
-  }
+ //mandar los valores
+ if(e.key === 'Enter') {
+  const item = new ToDoItem(false, description, 0);
 
-  // Mandar los valores
-  if(e.key === 'Enter' && description != '') {
-    const item = new ToDoItem(false, description);
+  //agregar un nuewvo toodo
+  Items.addItem(item);
 
-    // Agrear un nuevo todo
-    Items.addItem(item);
+  //limpiar la input
+  Items.clearInput();
+  
+ }
 
-    // Limpiar la input
-    Items.clearInput();
-  }
-});
 
-// Evento de escucha para remover elementos
-itemsContainer.addEventListener('click', (e) => {
-  // Remover Elemento
-  if(e.target.classList.contains('remove')){
-    Items.removeItem(e.target);
-  }
 });
